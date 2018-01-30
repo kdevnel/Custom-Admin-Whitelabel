0db699051d723488699b5bcf84efa1096d8748b9
Kyle Nel <kyle@devnel.com>
2018-01-30 15:21:07 +0000

    Initial commit with basic plugin boilerplate

---
+++ includes/class-custom-admin-whitelabel-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://devnel.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Admin_Whitelabel
+ * @subpackage Custom_Admin_Whitelabel/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Admin_Whitelabel
+ * @subpackage Custom_Admin_Whitelabel/includes
+ * @author     Kyle Nel <kyle@devnel.com>
+ */
+class Custom_Admin_Whitelabel_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/partials/custom-admin-whitelabel-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://devnel.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Admin_Whitelabel
+ * @subpackage Custom_Admin_Whitelabel/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
